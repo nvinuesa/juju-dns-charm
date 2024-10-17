3dc8ebe69838ae25170b523f6d39a540f35f2bdf
Nicolas Vinuesa <vinuesa.nico@gmail.com>
2024-10-17 15:39:07 +0200

    chore: clean up unused relation methods

--- src/charm.py
+++ src/charm.py
@@ -5,6 +5,7 @@
 from config import Config
 import logging
 from jinja2 import Template
+import yaml
 import subprocess
 from constants import CONTROLLER_RELATION, COREFILE_PATH, JUJU_DNS_SNAP_NAME, SNAP_PACKAGES,JUJU_DNS_PLUGIN_CONFIG_PATH
 import os
@@ -24,17 +25,17 @@ logger = logging.getLogger(__name__)
 
 class JujuDnsCharm(ops.CharmBase):
     _stored = StoredState()
+    controllers = {}
 
     def __init__(self, framework: ops.Framework):
         super().__init__(framework)
         framework.observe(self.on.start, self._on_start)
         framework.observe(self.on.install, self._on_install)
         framework.observe(self.on.config_changed, self._on_config_changed)
-        framework.observe(self.on.set_password_action, self._on_set_password)
-        framework.observe(self.on[CONTROLLER_RELATION].relation_joined, self._on_relation_joined)
-        self._stored.set_default(port=1053,ttl="60",username="",password="",address="",controller="")
+        self._stored.set_default(port=1053, ttl="60")
         port=ops.Port('udp', 1053)
         self.unit.set_ports(port)
+
 
     def _on_start(self, event: ops.StartEvent):
         """Handle start event."""
@@ -43,6 +44,29 @@ class JujuDnsCharm(ops.CharmBase):
     def _on_install(self, event: ops.InstallEvent):
         """Handle install event."""
         self.unit.status = ops.MaintenanceStatus("Installing juju-dns snap")
+
+        # Load controllers and accounts resources.
+        try:
+            accounts_path = self.model.resources.fetch("accounts")
+        except Exception as e:
+            logger.error("retrieving accounts.yaml file path from resources, reason: %s", str(e))
+            raise
+        try:
+            controllers_path = self.model.resources.fetch("controllers")
+        except Exception as e:
+            logger.error("retrieving controllers.yaml file path from resources, reason: %s", str(e))
+            raise
+
+        with open(accounts_path, "r") as file:
+            accounts_contents = yaml.safe_load(file)
+        with open(controllers_path, "r") as file:
+            controllers_contents = yaml.safe_load(file)
+
+        for controller_name in controllers_contents["controllers"]:
+            self.controllers[controller_name]={}
+            self.controllers[controller_name]["address"] = controllers_contents["controllers"][controller_name]["api-endpoints"][0]
+            self.controllers[controller_name]["username"] = accounts_contents["controllers"][controller_name]["user"]
+            self.controllers[controller_name]["password"] = accounts_contents["controllers"][controller_name]["password"]
 
         for snap_name, snap_version in SNAP_PACKAGES:
             try:
@@ -69,6 +93,9 @@ class JujuDnsCharm(ops.CharmBase):
                 )
                 raise
 
+        logger.info("controllers %r", self.controllers)
+        self._render_config()
+
         self.unit.status = ops.ActiveStatus("Ready")
 
     def _on_config_changed(self, event: ops.ConfigChangedEvent):
@@ -89,17 +116,6 @@ class JujuDnsCharm(ops.CharmBase):
             # Dump config yaml.
             self._render_config()
 
-    def _on_set_password(self, event: ActionEvent) -> None:
-        """Set the password for the specified user."""
-
-        username = event.params.get("username")
-        password = event.params.get("password")
-
-        if username != self._stored.username or password != self._stored.password:
-            self._stored.username=username
-            self._stored.password=password
-            self._render_config()
-
     def _render_config(self) -> None:
         """Render the juju-dns config file with the stored contents."""
 
@@ -108,10 +124,7 @@ class JujuDnsCharm(ops.CharmBase):
             template = Template(file.read())
 
         config = template.render(
-            controller=self._stored.controller,
-            address=self._stored.address,
-            username=self._stored.username,
-            password=self._stored.password,
+            controllers=self.controllers,
             ttl=self._stored.ttl
         )
         logger.info("Rendered config:\n%s", config)
@@ -147,43 +160,6 @@ class JujuDnsCharm(ops.CharmBase):
         juju_dns_snap = cache[JUJU_DNS_SNAP_NAME]
 
         juju_dns_snap.restart()
-
-    def _on_relation_joined(self, event: RelationJoinedEvent) -> None:
-        """Add peer to replica set.
-
-        Args:
-            event: The triggering relation joined event.
-        """
-
-        self._on_relation_handler(event)
-
-        # self._update_related_hosts(event)
-
-    def _on_relation_handler(self, event: RelationEvent) -> None:
-        """Update the controller address when joining the controller relation"""
-
-        for unit in event.relation.units:
-            if unit.app == "juju-dns":
-                # This is a peer unit
-                continue
-            if "port" in event.relation.data[unit]:
-                # We have found the leader unit (only leader unit should have
-                # the port in the relation data), so use this address and break.
-                self._stored.address = event.relation.data[unit]["private-address"]+":"+event.relation.data[unit]["port"]
-                break
-            # If no port is specified, use the default port.
-            self._stored.address = event.relation.data[unit]["private-address"]+":17070"
-
-        # Now that we have the address, also add the controller (model, because
-        # this charm is supposed to be deployed on the controller model) name
-        # to the config and render the file.
-        self._stored.controller = os.getenv["JUJU_CONTROLLER"]
-        self._render_config()
-
-
-    def _update_related_hosts(self, event) -> None:
-        # app relations should be made aware of the new set of hosts
-        return
 
 if __name__ == "__main__":  # pragma: nocover
     ops.main(JujuDnsCharm)  # type: ignore
